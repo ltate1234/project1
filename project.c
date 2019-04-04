65189be7db72191b310aae20f71550a4eb639c3b
Lachlan Tate <lachlan.tate@uon.edu.au>
2019-04-04 04:59:59 +0000

    changed to array

--- project.c
+++ project.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    int a = 0, b = 1, c = 2, d = 3, e = 4, f = 5, g = 6, h = 7, i = 8, j = 9, k = 10, l = 11, m = 12, n = 13, o = 14, p = 15, q = 16, r = 17, s = 18, t = 19, u = 20, v = 21, w = 22, x = 23, y = 24, z = 25;
+    char alphabet[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     int key = 2;
-    printf("%d ", m + key % 26);
-    printf("%d ", a + key % 26);
-    printf("%d ", n + key % 26);
+        printf("%c", alphabet[1] + key);
     
   return 0;
 } 
