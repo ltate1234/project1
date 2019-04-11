f1bb2a0c105a43533b799e832ac8b820f30ef4c4
ltate1234 <48460703+ltate1234@users.noreply.github.com>
2019-04-11 16:05:32 +1000

    Update project.c
    Doesn't work. big update because i dont know why che not working properly.

--- project.c
+++ project.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 
-int main()
+//protoype? maybe?
+
+void main()
 {
+    int key, z;
     char alphabet[100];
-    int key = 2;
-        printf("%c", alphabet[] + key);
-    //if() //havent had time yet
     
-  return 0;
+        printf("Enter Word: \n");
+        scanf("%d", &z);
+        printf("Enter a shift key: \n");
+        scanf("%d", &key)
+    for(z = 0; z < strlen(alphabet); z++);
+    {
+        if(alphabet[z] == ' '){
+        else if
+        {
+            alphabet[z] = alphabet[z] + key;
+            
+            if(alphabet[z] >= 'x')
+            {
+                alphabet[z] = alphabet[z] - 26;
+            }
+        }
+        }
+    }
+    
+  printf("Your encryption: ", alphabet);
 } 
+
+//function? maybe?
