3981eb727fdbe274aca8142c08af7b4df47e7e82
ltate1234 <48460703+ltate1234@users.noreply.github.com>
2019-04-11 16:48:52 +1000

    Update project.c
    i dont know ill figure it out later

--- project.c
+++ project.c
@@ -11,22 +11,6 @@ void main()
         scanf("%d", &z);
         printf("Enter a shift key: \n");
         scanf("%d", &key);
-    for(z = 0; z < 25; ++z);
-    {
-        if(alphabet[z] == '\0'){
-        else if()
-        {
-            alphabet[z] = alphabet[z] + key;
-            
-            if(alphabet[z] >= 'y')
-            {
-                alphabet[z] = alphabet[z] - 26;
-            }
-        }
-        }
-    }
-    
-  printf("Your encryption: %c", alphabet);
-} 
+}
 
 //function? maybe?
