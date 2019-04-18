a05c6e867b14422f5ac3b810b7beb2a4ca40c4a7
ltate1234 <48460703+ltate1234@users.noreply.github.com>
2019-04-18 17:30:39 +1000

    Update project.c
    added decryption. will fix final if statement later.

--- project.c
+++ project.c
@@ -3,16 +3,24 @@
  
 int main()
 {
+	int opt1;
+    
+	printf("Enter 1 for Cipher encryption: \nEnter 2 for Cipher decryption: \n");
+	scanf("%d", &opt1);
+    
+	if(opt1 == 1)
+	{
 	char letters[100];
 	int i, key;
-	
-	printf("Enter Key: ");
-	scanf("%d", &key);
 	
 	printf("Enter Characters: ");
 	scanf("%s", letters);
 	
-	if(key >= 26){
+	printf("Enter Shift Key: ");
+	scanf("%d", &key);
+	
+	if(key >= 26)
+	{
 	    key = key - 26;
 	}
 	
@@ -20,6 +28,34 @@ int main()
 	letters[i] = letters[i] + key;
 
 	printf("%s", letters);
+	}
+	
+	if(opt1 == 2)
+	{
+	    char letters[100];
+	int i, key;
+	
+	printf("Enter Characters: ");
+	scanf("%s", letters);
+	
+	printf("Enter Decryption Key: ");
+	scanf("%d", &key);
+	
+	if(key >= 26)
+	{
+	    key = key - 26;
+	}
+	
+	for(i = 0; i < 100 && letters[i] != '\0'; i++)
+	letters[i] = letters[i] - key;
+
+	printf("%s", letters);
+	}
+	
+	if(opt1 != 1 || 2)
+	{
+	    printf("Please enter a valid option.");
+	}
 	
 	return 0;
 }
