8955f99a152726ea622ae62ebda3fc7b377b225f
ltate1234 <48460703+ltate1234@users.noreply.github.com>
2019-04-25 00:13:09 +1000

    Update project.c
    edited some text and made it so the key will work with any positive number

--- project.c
+++ project.c
@@ -5,7 +5,7 @@ int main()
 {
     int opt1;
     
-    printf("Enter 1 for Rotation Cipher encryption. \nEnter 2 for Rotation Cipher decryption. \n");   //Prints the options
+    printf("Press 1 for Rotation Cipher Encryption. \nPress 2 for Rotation Cipher Decryption. \nPress 3 for Substitution Cipher Encryption. \nPress 4 for Substitution Cipher Decryption.");   //Prints the options to the console
     scanf("%d", &opt1);     //Scans for the user input and assigns it to opt1
     
     if(opt1 > 2 || opt1 < 1)
@@ -25,22 +25,22 @@ int main()
 	printf("Enter Characters: ");
 	scanf("%s", letters);
 	
-	printf("Enter Shift Key: ");
+	printf("Enter a rotation key between 0 and 25 (will shift to the right): ");
 	scanf("%d", &key);
 	
-	if(key >= 26)
+	if(key > 25)
 	{
-	    printf("Please enter a number between 0 and 25.\n");
-	    while(key >= 26)
+	    while(key > 25)
 	    {
-	        scanf("%d", &key);
+	        key = key - 25;
 	    }
 	}
 	
 	for(i = 0; i < 100 && letters[i] != '\0'; i++)
+	
 	letters[i] = letters[i] + key;
 
-	printf("%s", letters);
+	    printf("%s", letters);
 	}
 	
 	if(opt1 == 2)
@@ -51,19 +51,19 @@ int main()
 	printf("Enter Characters: ");
 	scanf("%s", letters);
 	
-	printf("Enter Decryption Key: ");
+	printf("Enter a decryption key between 0 and 25 (will shift to the left): ");
 	scanf("%d", &key);
 	
-	if(key >= 26)
+	if(key > 25)
 	{
-	    printf("Please enter a number between 0 and 25.\n");
-	    while(key >= 26)
+	    while(key > 25)
 	    {
-	        scanf("%d", &key);
+	        key = key - 25;
 	    }
 	}
 	
 	for(i = 0; i < 100 && letters[i] != '\0'; i++)
+	
 	letters[i] = letters[i] - key;
 
 	printf("%s", letters);
