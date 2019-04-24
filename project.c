2760216e71dc0b04cbf28808665cea892239d722
ltate1234 <48460703+ltate1234@users.noreply.github.com>
2019-04-25 00:27:48 +1000

    Update project.c
    tidied up

--- project.c
+++ project.c
@@ -5,48 +5,48 @@ int main()
 {
     int opt1;
     
-    printf("Press 1 for Rotation Cipher Encryption. \nPress 2 for Rotation Cipher Decryption. \nPress 3 for Substitution Cipher Encryption. \nPress 4 for Substitution Cipher Decryption.");   //Prints the options to the console
-    scanf("%d", &opt1);     //Scans for the user input and assigns it to opt1
+        printf("Press 1 for Rotation Cipher Encryption. \nPress 2 for Rotation Cipher Decryption. \nPress 3 for Substitution Cipher Encryption. \nPress 4 for Substitution Cipher Decryption. \n");   //Prints the options to the console
+        scanf("%d", &opt1);     //Scans for the user input and assigns it to opt1
     
     if(opt1 > 2 || opt1 < 1)
     {
         printf("Please enter valid option.\n");
-        while(opt1 > 2 || opt1 < 1)
-        {
-            scanf("%d", &opt1);
-        }
+            while(opt1 > 2 || opt1 < 1)
+            {
+                scanf("%d", &opt1);
+            }
     }
     
 	if(opt1 == 1)
 	{
-	char letters[100];
-	int i, key;
+	    char letters[100];
+	    int i, key;
 	
-	printf("Enter Characters: ");
-	scanf("%s", letters);
+	        printf("Enter Characters: ");
+	        scanf("%s", letters);
 	
-	printf("Enter a rotation key between 0 and 25 (will shift to the right): ");
-	scanf("%d", &key);
+	        printf("Enter a rotation key between 0 and 25 (will shift to the right): ");
+	        scanf("%d", &key);
 	
-	if(key > 25)
-	{
-	    while(key > 25)
+	    if(key > 25)
 	    {
-	        key = key - 25;
+	        while(key > 25)
+	        {
+	            key = key - 25;
+	        }
 	    }
-	}
 	
-	if(key < 0)
-	{
-	    while(key < 0)
+	    if(key < 0)
 	    {
-	        key = key + 25;
+	        while(key < 0)
+	        {
+	            key = key + 25;
+	        }
 	    }
-	}
 	
-	for(i = 0; i < 100 && letters[i] != '\0'; i++)
+	    for(i = 0; i < 100 && letters[i] != '\0'; i++)
 	
-	letters[i] = letters[i] + key;
+	    letters[i] = letters[i] + key;
 
 	    printf("%s", letters);
 	}
@@ -54,35 +54,35 @@ int main()
 	if(opt1 == 2)
 	{
 	    char letters[100];
-	int i, key;
+	    int i, key;
 	
-	printf("Enter Characters: ");
-	scanf("%s", letters);
+	        printf("Enter Characters: ");
+	        scanf("%s", letters);
 	
-	printf("Enter a decryption key between 0 and 25 (will shift to the left): ");
-	scanf("%d", &key);
+	        printf("Enter a decryption key between 0 and 25 (will shift to the left): ");
+	        scanf("%d", &key);
 	
-	if(key > 25)
-	{
-	    while(key > 25)
+	    if(key > 25)
 	    {
-	        key = key - 25;
+	        while(key > 25)
+	        {
+	            key = key - 25;
+	        }
 	    }
-	}
 	
-	if(key < 0)
-	{
-	    while(key < 0)
+	    if(key < 0)
 	    {
-	        key = key + 25;
+	        while(key < 0)
+	        {
+	            key = key + 25;
+	        }
 	    }
-	}
 	
-	for(i = 0; i < 100 && letters[i] != '\0'; i++)
+	    for(i = 0; i < 100 && letters[i] != '\0'; i++)
 	
-	letters[i] = letters[i] - key;
+	    letters[i] = letters[i] - key;
 
-	printf("%s", letters);
+	    printf("%s", letters);
 	}
 	
 	
